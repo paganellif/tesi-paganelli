63bd631300cc80324b01b3116765fa30054b88ef
Filippo Paganelli <filippo1paganelli@gmail.com>
2022-11-14 23:24:18 +0100

    Updates from Overleaf

---
+++ code/macos-runner-setup.sh
@@ -0,0 +1,19 @@
+# Download binario ufficiale
+sudo curl --output /usr/local/bin/gitlab-runner \
+    "https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/
+    gitlab-runner-darwin-arm64"
+# Cambio permessi per l'esecuzione del binario scaricato
+sudo chmod +x /usr/local/bin/gitlab-runner
+# Installazione runner
+gitlab-runner install
+# Registrazione runner
+sudo gitlab-runner register --non-interactive \
+  --url "https://gitlab.com/" --registration-token "MY-TOKEN" \
+  --executor "shell" --description "macos-runner" --request-concurrency 3\
+  --tag-list "macos-m1" --run-untagged="false" --locked="true" \
+  --access-level="not_protected" --cache-shared --cache-type="gcs" \
+  --cache-gcs-access-id="${CACHE_GCS_ACCESS_ID}" \
+  --cache-gcs-private-key="${CACHE_GCS_PRIVATE_KEY}" \
+  --cache-gcs-bucket-name="${CACHE_GCS_PRIVATE_NAME}"
+# Start Runner
+gitlab-runner start
