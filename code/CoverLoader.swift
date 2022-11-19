d82e51e035ec3c8cbe1263f1be909bb9b8d11e9b
Filippo Paganelli <filippo1paganelli@gmail.com>
2022-11-19 11:30:08 +0100

    Updates from Overleaf

---
+++ code/CoverLoader.swift
@@ -0,0 +1,19 @@
+import shared
+
+final class CoverLoader: ObservableObject {
+    @Published var data: Data?
+    private let bookCover = GetBookCoverUseCase.init()
+    
+    init(_ isbn: String) {
+        self.bookCover.invokeNative(isbn: isbn) { result in
+            DispatchQueue.main.async {
+                if result != nil {
+                    self.data = ByteArrayConverter.init().toData(byteArray: result!)
+                }
+            }
+        } onError: { throwable in
+            print(throwable.getStackTrace())
+            self.data = Data()
+        }
+    }
+}
