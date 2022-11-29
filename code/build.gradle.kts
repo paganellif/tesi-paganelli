1d7e14b871df9b030bd7c567f7e495a4f91c3b8e
Filippo Paganelli <filippo1paganelli@gmail.com>
2022-11-29 09:37:30 +0100

    Updates from Overleaf

--- code/build.gradle.kts
+++ code/build.gradle.kts
@@ -1,32 +1,23 @@
 plugins {
-    kotlin("multiplatform") // org.jetbrains.kotlin.multiplatform
-    id("com.android.library")
-    kotlin("native.cocoapods") // org.jetbrains.kotlin.native.cocoapods
+  kotlin("multiplatform") // org.jetbrains.kotlin.multiplatform
+  id("com.android.library")
+  kotlin("native.cocoapods") // org.jetbrains.kotlin.native.cocoapods
 }
 
 kotlin {
-    android()
-    iosArm64()
-    cocoapods {  // Configurazione Cocoapods
-        ios.deploymentTarget = "14.1"
-        podfile = project.file("../iosApp/Podfile")
-        framework {
-            baseName = "shared"
-        }
-    }
-    sourceSets {
-        val commonMain by getting {
-            dependencies { } // Dipendenze comuni a Android e iOS
-        }
+  android()
+  iosArm64()
+  cocoapods {  // Configurazione Cocoapods
+    ios.deploymentTarget = "14.1"
+    podfile = project.file("../iosApp/Podfile")
+    framework { baseName = "shared" }
+  }
 
-        val androidMain by getting {
-            dependencies { } // Dipendenze specifiche Android
-        }
-
-        val iosMain by creating {
-            dependencies { } // Dipendenze specifiche iOS
-        }
-    }
+  sourceSets {
+    val commonMain by getting { dependencies {} } // Dipendenze comuni Android e iOS
+    val androidMain by getting { dependencies {} } // Dipendenze specifiche Android
+    val iosMain by creating { dependencies {} } // Dipendenze specifiche iOS
+  }
 }
 
 android { } // Configurazione android 
